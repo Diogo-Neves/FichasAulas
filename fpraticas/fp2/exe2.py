c6c5b6099d89abce52ded6dccff0a52a2546d0e6
Diogo-Neves <diogo_neves2009@hotmail.com>
2018-03-13 15:25:56 +0000

    Exe2 de novo

--- fpraticas/fp2/exe2.py
+++ fpraticas/fp2/exe2.py
@@ -4,10 +4,10 @@
 numero1=int(input("Insira um valor:"))
 numero2=int(input("insira outro:"))
 
-if numero1 > numero2
+if numero1 > numero2:
     print(numero1 , numero2)
-elif numero2 > numero1
+elif numero2 > numero1:
     print(numero2 , numero1 )
-elif numero1 == numero2
+elif numero1 == numero2:
     print("Os numeros são iguais")
-    
+
