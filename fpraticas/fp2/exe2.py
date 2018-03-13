7fce09170b7f39a22c31bb78dd3ab08a14005e4b
Diogo-Neves <diogo_neves2009@hotmail.com>
2018-03-13 14:58:35 +0000

    Ficha 2 feita

---
+++ fpraticas/fp2/exe2.py
@@ -0,0 +1,13 @@
+
+#ler dois valores numericos e ordena-los do maior pro menor
+
+numero1=int(input("Insira um valor:"))
+numero2=int(input("insira outro:"))
+
+if numero1 > numero2
+    print(numero1 , numero2)
+elif numero2 > numero1
+    print(numero2 , numero1 )
+elif numero1 == numero2
+    print("Os numeros são iguais")
+    
